83c9b1b95fd3a1537243366c66e51f2ea2126301
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-07-17 10:26:48 -0500

    fix non-null assert

--- playground/index.tsx
+++ playground/index.tsx
@@ -1,5 +1,5 @@
 import * as ReactDOM from "react-dom/client"
 import { UserMenu } from "../src/index.tsx"
 
-const root = ReactDOM.createRoot(document.getElementById("root")!)
+const root = ReactDOM.createRoot(document.querySelector("#root") as HTMLElement)
 root.render(<UserMenu />)
